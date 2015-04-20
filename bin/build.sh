24d9f9efc8bb4963e7e48266d80fd3ca9b1b0a86
Selene ToyKeeper <git@toykeeper.net>
2015-04-19 23:20:03 -0600

    Added "-c -std=gnu99" to build script, made some small 'inline' changes to sources, and made output even smaller.

--- bin/build.sh
+++ bin/build.sh
@@ -7,7 +7,8 @@ export PROGRAM=$1
 export MCU=attiny13
 export CC=avr-gcc
 export OBJCOPY=avr-objcopy
-export CFLAGS="-Wall -g -Os -mmcu=$MCU"
+export CFLAGS="-Wall -g -Os -mmcu=$MCU -c -std=gnu99"
+export OFLAGS="-Wall -g -Os -mmcu=$MCU"
 export LDFLAGS=
 export OBJCOPYFLAGS='--set-section-flags=.eeprom=alloc,load --change-section-lma .eeprom=0 --no-change-warnings -O ihex'
 export OBJS=$PROGRAM.o
@@ -19,6 +20,6 @@ function run () {
 }
 
 run $CC $CFLAGS -o $PROGRAM.o -c $PROGRAM.c
-run $CC $CFLAGS $LDFLAGS -o $PROGRAM.elf $PROGRAM.o
+run $CC $OFLAGS $LDFLAGS -o $PROGRAM.elf $PROGRAM.o
 run $OBJCOPY $OBJCOPYFLAGS $PROGRAM.elf $PROGRAM.hex
 run avr-size -C --mcu=attiny13 $PROGRAM.elf | grep Full
