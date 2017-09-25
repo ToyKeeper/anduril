fdca655eb1802da86cba79c4e605d6d8acecbfa3
Selene ToyKeeper <git@toykeeper.net>
2017-09-24 22:47:00 -0600

    Reduced standby power by 700X by turning off brownout detection. (from 24.7 uA to 0.35 uA)

--- bin/flash-85.sh
+++ bin/flash-85.sh
@@ -1,4 +1,4 @@
 #/bin/sh
 FIRMWARE=$1
-avrdude -c usbasp -p t85 -u -U lfuse:w:0xe2:m -U hfuse:w:0xde:m -U efuse:w:0xff:m -Uflash:w:$FIRMWARE
+avrdude -c usbasp -p t85 -u -U lfuse:w:0xe2:m -U hfuse:w:0xdf:m -U efuse:w:0xff:m -Uflash:w:$FIRMWARE
 # http://www.engbedded.com/cgi-bin/fcx.cgi?P_PREV=ATtiny85&P=ATtiny85&M_LOW_0x3F=0x22&M_HIGH_0x07=0x06&M_HIGH_0x20=0x00&B_SPIEN=P&B_SUT0=P&B_CKSEL3=P&B_CKSEL2=P&B_CKSEL0=P&B_BODLEVEL0=P&V_LOW=E2&V_HIGH=DE&V_EXTENDED=FF
