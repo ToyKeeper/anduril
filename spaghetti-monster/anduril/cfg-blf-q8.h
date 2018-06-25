809a397855a2a5ff9fdb327a7a30aa639f829b51
Selene ToyKeeper <git@toykeeper.net>
2018-06-24 18:55:19 -0600

    Moved driver-specific details to separate header files. General code cleanup and notes. Fixed build when thermal regulation is turned off. May have fixed (or at least improved) issue with main LED interfering with aux LEDs during aux LED config.

---
+++ spaghetti-monster/anduril/cfg-blf-q8.h
@@ -0,0 +1,9 @@
+// the button lights up
+#define USE_INDICATOR_LED
+// the button is visible while main LEDs are on
+#define USE_INDICATOR_LED_WHILE_RAMPING
+// enable blinking indicator LED while off
+#define TICK_DURING_STANDBY
+
+// average drop across diode on this hardware
+#define VOLTAGE_FUDGE_FACTOR 7  // add 0.35V
---
+++ spaghetti-monster/anduril/cfg-emisar-d4.h
@@ -0,0 +1,2 @@
+// average drop across diode on this hardware
+#define VOLTAGE_FUDGE_FACTOR 5  // add 0.25V
