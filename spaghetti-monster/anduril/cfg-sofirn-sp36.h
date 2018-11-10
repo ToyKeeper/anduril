387cb35e8f9e2ca74cfc019d06a0051bc7b11aea
Selene ToyKeeper <git@toykeeper.net>
2018-11-10 01:13:42 -0700

    calibrated Sofirn SP36 config

--- spaghetti-monster/anduril/cfg-sofirn-sp36.h
+++ spaghetti-monster/anduril/cfg-sofirn-sp36.h
@@ -1,16 +1,34 @@
 // Sofirn SP36 (small Q8) config options for Anduril
 
+// voltage readings were a little high with the Q8 value
+#define VOLTAGE_FUDGE_FACTOR 5  // add 0.25V, not 0.35V
+
 #define FSM_BLF_Q8_DRIVER
 #include "cfg-blf-q8.h"
 
-// stop panicking at ~50% power or ~2000 lm
+// the high button LED mode on this light uses too much power
+// off mode: low (1)
+// lockout: blinking (3)
+#define INDICATOR_LED_DEFAULT_MODE ((3<<2) + 1)
+
+// don't blink during the ramp; the button LED brightness is sufficient
+// to indicate which power channel(s) are being used
+#ifdef BLINK_AT_CHANNEL_BOUNDARIES
+#undef BLINK_AT_CHANNEL_BOUNDARIES
+#endif
+#ifdef BLINK_AT_RAMP_CEILING
+#undef BLINK_AT_RAMP_CEILING
+#endif
+
+// stop panicking at ~60% power or ~3000 lm
 #ifdef THERM_FASTER_LEVEL
 #undef THERM_FASTER_LEVEL
 #endif
-#define THERM_FASTER_LEVEL 125
+#define THERM_FASTER_LEVEL 130
 
 // be extra-careful at high levels
-#ifndef THERM_HARD_TURBO_DROP
-#define THERM_HARD_TURBO_DROP
-#endif
+// (or not... this host seems to heat up pretty slowly)
+//#ifndef THERM_HARD_TURBO_DROP
+//#define THERM_HARD_TURBO_DROP
+//#endif
 
