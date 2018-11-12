ec34acd89d80c5a26c68e873a006f602a2d9e598
Selene ToyKeeper <git@toykeeper.net>
2018-11-11 18:02:40 -0700

    Made event definitions a little cleaner.

--- spaghetti-monster/fsm-events.h
+++ spaghetti-monster/fsm-events.h
@@ -89,115 +89,115 @@ static volatile uint16_t ticks_since_last_event = 0;
 
 // Button press events
 
-// shouldn't normally happen, but UI might reset event while button is down
-// so a release with no recorded prior hold could be possible
+// shouldn't normally happen, but UI might empty_event_sequence() while button
+// is down so a release with no recorded prior hold could be possible
 #define EV_release             (B_CLICK|B_RELEASE|0)
 
 #define EV_click1_press        (B_CLICK|B_PRESS|1)
-#define EV_click1_hold         (B_CLICK|B_HOLD|B_PRESS|1)
-#define EV_click1_hold_release (B_CLICK|B_TIMEOUT|B_HOLD|B_RELEASE|1)
 #define EV_click1_release      (B_CLICK|B_RELEASE|1)
 #define EV_click1_complete     (B_CLICK|B_TIMEOUT|1)
 #define EV_1click EV_click1_complete
+#define EV_click1_hold         (B_CLICK|B_HOLD|B_PRESS|1)
+#define EV_click1_hold_release (B_CLICK|B_HOLD|B_RELEASE|B_TIMEOUT|1)
 #define EV_hold EV_click1_hold
 
 #define EV_click2_press        (B_CLICK|B_PRESS|2)
-#define EV_click2_hold         (B_CLICK|B_HOLD|B_PRESS|2)
-#define EV_click2_hold_release (B_CLICK|B_TIMEOUT|B_HOLD|B_RELEASE|2)
 #define EV_click2_release      (B_CLICK|B_RELEASE|2)
 #define EV_click2_complete     (B_CLICK|B_TIMEOUT|2)
 #define EV_2clicks EV_click2_complete
+#define EV_click2_hold         (B_CLICK|B_HOLD|B_PRESS|2)
+#define EV_click2_hold_release (B_CLICK|B_HOLD|B_RELEASE|B_TIMEOUT|2)
 
 #define EV_click3_press        (B_CLICK|B_PRESS|3)
-#define EV_click3_hold         (B_CLICK|B_HOLD|B_PRESS|3)
-#define EV_click3_hold_release (B_CLICK|B_TIMEOUT|B_HOLD|B_RELEASE|3)
 #define EV_click3_release      (B_CLICK|B_RELEASE|3)
 #define EV_click3_complete     (B_CLICK|B_TIMEOUT|3)
 #define EV_3clicks EV_click3_complete
+#define EV_click3_hold         (B_CLICK|B_HOLD|B_PRESS|3)
+#define EV_click3_hold_release (B_CLICK|B_HOLD|B_RELEASE|B_TIMEOUT|3)
 
 #define EV_click4_press        (B_CLICK|B_PRESS|4)
-#define EV_click4_hold         (B_CLICK|B_HOLD|B_PRESS|4)
-#define EV_click4_hold_release (B_CLICK|B_TIMEOUT|B_HOLD|B_RELEASE|4)
 #define EV_click4_release      (B_CLICK|B_RELEASE|4)
 #define EV_click4_complete     (B_CLICK|B_TIMEOUT|4)
 #define EV_4clicks EV_click4_complete
+#define EV_click4_hold         (B_CLICK|B_HOLD|B_PRESS|4)
+#define EV_click4_hold_release (B_CLICK|B_HOLD|B_RELEASE|B_TIMEOUT|4)
 
 #define EV_click5_press        (B_CLICK|B_PRESS|5)
-#define EV_click5_hold         (B_CLICK|B_HOLD|B_PRESS|5)
-#define EV_click5_hold_release (B_CLICK|B_TIMEOUT|B_HOLD|B_RELEASE|5)
 #define EV_click5_release      (B_CLICK|B_RELEASE|5)
 #define EV_click5_complete     (B_CLICK|B_TIMEOUT|5)
 #define EV_5clicks EV_click5_complete
+#define EV_click5_hold         (B_CLICK|B_HOLD|B_PRESS|5)
+#define EV_click5_hold_release (B_CLICK|B_HOLD|B_RELEASE|B_TIMEOUT|5)
 
 #define EV_click6_press        (B_CLICK|B_PRESS|6)
-#define EV_click6_hold         (B_CLICK|B_HOLD|B_PRESS|6)
-#define EV_click6_hold_release (B_CLICK|B_TIMEOUT|B_HOLD|B_RELEASE|6)
 #define EV_click6_release      (B_CLICK|B_RELEASE|6)
 #define EV_click6_complete     (B_CLICK|B_TIMEOUT|6)
 #define EV_6clicks EV_click6_complete
+#define EV_click6_hold         (B_CLICK|B_HOLD|B_PRESS|6)
+#define EV_click6_hold_release (B_CLICK|B_HOLD|B_RELEASE|B_TIMEOUT|6)
 
 #define EV_click7_press        (B_CLICK|B_PRESS|7)
-#define EV_click7_hold         (B_CLICK|B_HOLD|B_PRESS|7)
-#define EV_click7_hold_release (B_CLICK|B_TIMEOUT|B_HOLD|B_RELEASE|7)
 #define EV_click7_release      (B_CLICK|B_RELEASE|7)
 #define EV_click7_complete     (B_CLICK|B_TIMEOUT|7)
 #define EV_7clicks EV_click7_complete
+#define EV_click7_hold         (B_CLICK|B_HOLD|B_PRESS|7)
+#define EV_click7_hold_release (B_CLICK|B_HOLD|B_RELEASE|B_TIMEOUT|7)
 
 #define EV_click8_press        (B_CLICK|B_PRESS|8)
-#define EV_click8_hold         (B_CLICK|B_HOLD|B_PRESS|8)
-#define EV_click8_hold_release (B_CLICK|B_TIMEOUT|B_HOLD|B_RELEASE|8)
 #define EV_click8_release      (B_CLICK|B_RELEASE|8)
 #define EV_click8_complete     (B_CLICK|B_TIMEOUT|8)
 #define EV_8clicks EV_click8_complete
+#define EV_click8_hold         (B_CLICK|B_HOLD|B_PRESS|8)
+#define EV_click8_hold_release (B_CLICK|B_HOLD|B_RELEASE|B_TIMEOUT|8)
 
 #define EV_click9_press        (B_CLICK|B_PRESS|9)
-#define EV_click9_hold         (B_CLICK|B_HOLD|B_PRESS|9)
-#define EV_click9_hold_release (B_CLICK|B_TIMEOUT|B_HOLD|B_RELEASE|9)
 #define EV_click9_release      (B_CLICK|B_RELEASE|9)
 #define EV_click9_complete     (B_CLICK|B_TIMEOUT|9)
 #define EV_9clicks EV_click9_complete
+#define EV_click9_hold         (B_CLICK|B_HOLD|B_PRESS|9)
+#define EV_click9_hold_release (B_CLICK|B_HOLD|B_RELEASE|B_TIMEOUT|9)
 
 #define EV_click10_press        (B_CLICK|B_PRESS|10)
-#define EV_click10_hold         (B_CLICK|B_HOLD|B_PRESS|10)
-#define EV_click10_hold_release (B_CLICK|B_TIMEOUT|B_HOLD|B_RELEASE|10)
 #define EV_click10_release      (B_CLICK|B_RELEASE|10)
 #define EV_click10_complete     (B_CLICK|B_TIMEOUT|10)
 #define EV_10clicks EV_click10_complete
+#define EV_click10_hold         (B_CLICK|B_HOLD|B_PRESS|10)
+#define EV_click10_hold_release (B_CLICK|B_HOLD|B_RELEASE|B_TIMEOUT|10)
 
 #define EV_click11_press        (B_CLICK|B_PRESS|11)
-#define EV_click11_hold         (B_CLICK|B_HOLD|B_PRESS|11)
-#define EV_click11_hold_release (B_CLICK|B_TIMEOUT|B_HOLD|B_RELEASE|11)
 #define EV_click11_release      (B_CLICK|B_RELEASE|11)
 #define EV_click11_complete     (B_CLICK|B_TIMEOUT|11)
 #define EV_11clicks EV_click11_complete
+#define EV_click11_hold         (B_CLICK|B_HOLD|B_PRESS|11)
+#define EV_click11_hold_release (B_CLICK|B_HOLD|B_RELEASE|B_TIMEOUT|11)
 
 #define EV_click12_press        (B_CLICK|B_PRESS|12)
-#define EV_click12_hold         (B_CLICK|B_HOLD|B_PRESS|12)
-#define EV_click12_hold_release (B_CLICK|B_TIMEOUT|B_HOLD|B_RELEASE|12)
 #define EV_click12_release      (B_CLICK|B_RELEASE|12)
 #define EV_click12_complete     (B_CLICK|B_TIMEOUT|12)
 #define EV_12clicks EV_click12_complete
+#define EV_click12_hold         (B_CLICK|B_HOLD|B_PRESS|12)
+#define EV_click12_hold_release (B_CLICK|B_HOLD|B_RELEASE|B_TIMEOUT|12)
 
 #define EV_click13_press        (B_CLICK|B_PRESS|13)
-#define EV_click13_hold         (B_CLICK|B_HOLD|B_PRESS|13)
-#define EV_click13_hold_release (B_CLICK|B_TIMEOUT|B_HOLD|B_RELEASE|13)
 #define EV_click13_release      (B_CLICK|B_RELEASE|13)
 #define EV_click13_complete     (B_CLICK|B_TIMEOUT|13)
 #define EV_13clicks EV_click13_complete
+#define EV_click13_hold         (B_CLICK|B_HOLD|B_PRESS|13)
+#define EV_click13_hold_release (B_CLICK|B_HOLD|B_RELEASE|B_TIMEOUT|13)
 
 #define EV_click14_press        (B_CLICK|B_PRESS|14)
-#define EV_click14_hold         (B_CLICK|B_HOLD|B_PRESS|14)
-#define EV_click14_hold_release (B_CLICK|B_TIMEOUT|B_HOLD|B_RELEASE|14)
 #define EV_click14_release      (B_CLICK|B_RELEASE|14)
 #define EV_click14_complete     (B_CLICK|B_TIMEOUT|14)
 #define EV_14clicks EV_click14_complete
+#define EV_click14_hold         (B_CLICK|B_HOLD|B_PRESS|14)
+#define EV_click14_hold_release (B_CLICK|B_HOLD|B_RELEASE|B_TIMEOUT|14)
 
 #define EV_click15_press        (B_CLICK|B_PRESS|15)
-#define EV_click15_hold         (B_CLICK|B_HOLD|B_PRESS|15)
-#define EV_click15_hold_release (B_CLICK|B_TIMEOUT|B_HOLD|B_RELEASE|15)
 #define EV_click15_release      (B_CLICK|B_RELEASE|15)
 #define EV_click15_complete     (B_CLICK|B_TIMEOUT|15)
 #define EV_15clicks EV_click15_complete
+#define EV_click15_hold         (B_CLICK|B_HOLD|B_PRESS|15)
+#define EV_click15_hold_release (B_CLICK|B_HOLD|B_RELEASE|B_TIMEOUT|15)
 
 
 void empty_event_sequence();
