02da00884b824ee5fefcd4c64096821ce32888b4
Selene ToyKeeper <git@toykeeper.net>
2020-04-27 17:54:00 -0600

    calibrated K1 and K1-12V thermal response (smaller, slower) and candle mode (wobblier), and gave K1-12V a lower moon mode

--- spaghetti-monster/anduril/cfg-noctigon-k1.h
+++ spaghetti-monster/anduril/cfg-noctigon-k1.h
@@ -21,6 +21,7 @@
 
 // ../../bin/level_calc.py cube 1 150 7135 1 4 1300
 // (with max_pwm set to 1023)
+// (level 0 flickers and isn't relevant on a thrower, so it's omitted)
 #define RAMP_LENGTH 150
 #define PWM1_LEVELS 1,1,2,2,3,3,4,4,5,6,6,7,8,9,10,11,12,13,14,15,16,17,18,20,21,23,24,26,27,29,31,32,34,36,38,40,43,45,47,49,52,54,57,60,62,65,68,71,74,77,81,84,87,91,95,98,102,106,110,114,118,122,127,131,136,141,145,150,155,160,166,171,176,182,188,193,199,205,211,218,224,231,237,244,251,258,265,272,280,287,295,303,310,319,327,335,344,352,361,370,379,388,397,407,416,426,436,446,457,467,477,488,499,510,521,533,544,556,568,580,592,604,617,629,642,655,668,682,695,709,723,737,751,766,781,795,810,826,841,857,872,888,904,921,937,954,971,988,1005,1023
 #define MAX_1x7135 50
@@ -46,8 +47,14 @@
 #define MUGGLE_FLOOR RAMP_DISCRETE_FLOOR
 #define MUGGLE_CEILING 70
 
+// make candle mode wobble more
+#define CANDLE_AMPLITUDE 32
+
 // stop panicking at ~70% power or ~600 lm
 #define THERM_FASTER_LEVEL 130
+
+#define THERM_RESPONSE_MAGNITUDE 32  // smaller adjustments, this host changes temperature slowly
+#define THERM_NEXT_WARNING_THRESHOLD 32  // more error tolerance before adjusting
 
 // easier access to thermal config mode, for Noctigon
 #define USE_TENCLICK_THERMAL_CONFIG
