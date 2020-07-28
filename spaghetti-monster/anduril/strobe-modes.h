cbff464641e03de4eb24542167e29f9b36f85f86
Selene ToyKeeper <git@toykeeper.net>
2020-07-28 01:33:42 -0600

    moved candle mode to its own file, since it's kinda big

--- spaghetti-monster/anduril/strobe-modes.h
+++ spaghetti-monster/anduril/strobe-modes.h
@@ -86,12 +86,7 @@ inline void bike_flasher_iter();
 #endif
 
 #ifdef USE_CANDLE_MODE
-uint8_t candle_mode_state(Event event, uint16_t arg);
-// moved to fsm-misc.c because tint ramping power correction
-//uint8_t triangle_wave(uint8_t phase);
-#ifndef CANDLE_AMPLITUDE
-#define CANDLE_AMPLITUDE 25
-#endif
+#include "candle-mode.h"
 #endif
 
 
