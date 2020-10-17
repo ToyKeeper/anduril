f8c6e2edd5fbfc9e54d6072e1ab53dd887968ad5
Selene ToyKeeper <git@toykeeper.net>
2020-10-16 22:11:25 -0600

    added K9.3-noFET build target (for models with E21A as main LEDs)

--- spaghetti-monster/anduril/cfg-noctigon-k9.3.h
+++ spaghetti-monster/anduril/cfg-noctigon-k9.3.h
@@ -49,7 +49,7 @@ inline void set_level_override(uint8_t level);
 #define HALFSPEED_LEVEL 10
 #define QUARTERSPEED_LEVEL 2
 
-#define RAMP_SMOOTH_FLOOR 3  // level 1 is unreliable
+#define RAMP_SMOOTH_FLOOR 3  // level 1 is unreliable (?)
 #define RAMP_SMOOTH_CEIL  120
 // 10, 28, [46], 65, 83, 101, [120]
 #define RAMP_DISCRETE_FLOOR 10
