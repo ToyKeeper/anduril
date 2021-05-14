3c6c331ba52646c978c6f84bc93e7b45f9b9c58e
Selene ToyKeeper <git@toykeeper.net>
2021-05-13 22:20:27 -0600

    oops, somehow I forgot to add this earlier...  Hank's UI preferences (applies to all Emisar / Noctigon lights)

---
+++ spaghetti-monster/anduril/hank-cfg.h
@@ -0,0 +1,21 @@
+#ifndef HANK_CFG
+#define HANK_CFG
+
+// config preferences for Hank Wang of Intl-Outdoor (Emisar, Noctigon)
+
+// RGB aux LEDs should use rainbow cycling mode
+// to impress new customers
+// (people usually change it to voltage mode later though)
+#ifdef RGB_LED_OFF_DEFAULT
+#undef RGB_LED_OFF_DEFAULT
+#endif
+#define RGB_LED_OFF_DEFAULT 0x18  // low, rainbow
+
+// half a second per color in rainbow mode
+//#define RGB_RAINBOW_SPEED 0x03
+
+
+// double click while on goes to full-power turbo, not ramp ceiling
+#define USE_2C_MAX_TURBO
+
+#endif  // ifndef HANK_CFG
