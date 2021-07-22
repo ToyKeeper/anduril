2b6e0240465aee32b14e5dfba98b3d4a1567e2bd
Selene ToyKeeper <git@toykeeper.net>
2021-07-22 10:14:07 -0600

    summarize results at end of batch build process

--- spaghetti-monster/anduril/build-all.sh
+++ spaghetti-monster/anduril/build-all.sh
@@ -4,12 +4,41 @@ UI=anduril
 
 date '+#define VERSION_NUMBER "%Y%m%d"' > version.h
 
+PASS=0
+FAIL=0
+PASSED=''
+FAILED=''
+
 for TARGET in cfg-*.h ; do
+
+  # friendly name for this build
   NAME=$(echo "$TARGET" | perl -ne '/cfg-(.*).h/ && print "$1\n";')
   echo "===== $NAME ====="
+
+  # figure out MCU type
   ATTINY=$(grep 'ATTINY:' $TARGET | awk '{ print $3 }')
   if [ -z "$ATTINY" ]; then ATTINY=85 ; fi
+
+  # try to compile
   echo ../../../bin/build.sh $ATTINY "$UI" "-DCONFIGFILE=${TARGET}"
   ../../../bin/build.sh $ATTINY "$UI" "-DCONFIGFILE=${TARGET}"
-  mv -f "$UI".hex "$UI".$NAME.hex
+
+  # track result, and rename compiled files
+  if [ 0 = $? ] ; then
+    mv -f "$UI".hex "$UI".$NAME.hex
+    PASS=$(($PASS + 1))
+    PASSED="$PASSED $NAME"
+  else
+    echo "ERROR: build failed"
+    FAIL=$(($FAIL + 1))
+    FAILED="$FAILED $NAME"
+  fi
+
 done
+
+# summary
+echo "===== $PASS builds succeeded, $FAIL failed ====="
+#echo "PASS: $PASSED"
+if [ 0 != $FAIL ]; then
+  echo "FAIL:$FAILED"
+fi
