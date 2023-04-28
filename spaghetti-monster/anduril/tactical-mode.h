8b77e295c448d7f5ecc2cb34829f84dac648ce2b
Selene ToyKeeper <git@toykeeper.net>
2023-04-28 02:47:45 -0600

    only use 2-color strobe in tactical mode if it's on main LEDs, not aux

--- spaghetti-monster/anduril/tactical-mode.h
+++ spaghetti-monster/anduril/tactical-mode.h
@@ -6,9 +6,12 @@
 
 #ifndef TACTICAL_LEVELS
     // high, low, tactical strobe
-    #ifdef USE_POLICE_COLOR_STROBE_MODE
+    // only do color strobe here if it's main LEDs, not aux LEDs
+    #if defined(USE_POLICE_COLOR_STROBE_MODE) && !defined(POLICE_STROBE_USES_AUX)
+        // 2-color police style strobe
         #define TACTICAL_LEVELS 120,30,(RAMP_SIZE+3)
     #else
+        // regular tactical strobe (1 color)
         #define TACTICAL_LEVELS 120,30,(RAMP_SIZE+2)
     #endif
 #endif
