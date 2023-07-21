188eb9e0f2a31e07d5faee27c3f42dacb818e46e
Selene ToyKeeper <git@toykeeper.net>
2023-07-21 15:44:38 -0600

    converted emisar-d4v2-nofet to multi-channel, and gave it a new ramp with dynamic PWM

---
+++ hwdef-emisar-d4v2-nofet.c
@@ -0,0 +1,55 @@
+// Emisar D4v2 (no DD FET, 1x7135 only) PWM helper functions
+// Copyright (C) 2017-2023 Selene ToyKeeper
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+#include "chan-rgbaux.c"
+
+void set_level_main(uint8_t level);
+bool gradual_tick_main(uint8_t gt);
+
+
+Channel channels[] = {
+    { // main LEDs
+        .set_level    = set_level_main,
+        .gradual_tick = gradual_tick_main
+    },
+    RGB_AUX_CHANNELS
+};
+
+
+// single set of LEDs with just a 1x7135 chip, max 350 mA or ~130 lm
+void set_level_main(uint8_t level) {
+    if (level == 0) {
+        CH1_PWM = 0;
+        PWM_CNT = 0;  // reset phase
+        return;
+    }
+
+    level --;  // PWM array index = level - 1
+    PWM_DATATYPE ch1_pwm = PWM_GET(pwm1_levels, level);
+    // pulse frequency modulation, a.k.a. dynamic PWM
+    uint16_t top = PWM_GET16(pwm_tops, level);
+
+    CH1_PWM = ch1_pwm;
+    // wait to sync the counter and avoid flashes
+    while(actual_level && (PWM_CNT > (top - 32))) {}
+    PWM_TOP = top;
+    // force reset phase when turning on from zero
+    // (because otherwise the initial response is inconsistent)
+    if (! actual_level) PWM_CNT = 0;
+}
+
+bool gradual_tick_main(uint8_t gt) {
+    PWM_DATATYPE pwm1 = PWM_GET(pwm1_levels, gt);
+
+    GRADUAL_ADJUST_SIMPLE (pwm1, CH1_PWM);
+
+    if (   (pwm1 == CH1_PWM)
+       ) {
+        return true;  // done
+    }
+    return false;  // not done yet
+}
+
