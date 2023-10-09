4b6d6be3b26460bb81b41efcae63cac3a578dfb7
Selene ToyKeeper <git@toykeeper.net>
2023-10-09 10:54:18 -0600

    converted old MF01S / MT18S build

---
+++ hwdef-mateminco-mf01s.h
@@ -0,0 +1,101 @@
+// Mateminco MT18S / Astrolux MF01S driver layout
+// Copyright (C) 2019-2023 Selene ToyKeeper
+// SPDX-License-Identifier: GPL-3.0-or-later
+#pragma once
+
+/*
+ *           ----
+ *   Reset -|1  8|- VCC (unused)
+ * eswitch -|2  7|- Voltage divider (2S)
+ * AUX LED -|3  6|- PWM (FET)
+ *     GND -|4  5|- PWM (smaller FET)
+ *           ----
+ */
+
+#define ATTINY 85
+#include <avr/io.h>
+
+#define HWDEF_C_FILE hwdef-emisar-d4.c
+
+// channel modes
+// * 0. FET+7135 stacked
+#define NUM_CHANNEL_MODES  1
+enum CHANNEL_MODES {
+    CM_MAIN = 0,
+};
+
+#define DEFAULT_CHANNEL_MODE  CM_MAIN
+
+// right-most bit first, modes are in fedcba9876543210 order
+#define CHANNEL_MODES_ENABLED 0b00000001
+
+
+#define PWM_CHANNELS 2  // old, remove this
+
+#define PWM_BITS      8       // attiny85 only supports up to 8 bits
+#define PWM_GET       PWM_GET8
+#define PWM_DATATYPE  uint8_t
+#define PWM_DATATYPE2 uint16_t
+#define PWM1_DATATYPE uint8_t   // little FET ramp
+#define PWM2_DATATYPE uint8_t   // big FET ramp
+
+#define PWM_TOP_INIT  255    // highest value used in top half of ramp
+
+// little FET channel
+#define CH1_PIN  PB0        // pin 5, 1x7135 PWM
+#define CH1_PWM  OCR0A      // OCR0A is the output compare register for PB0
+
+// big FET channel
+#define CH2_PIN  PB1        // pin 6, FET PWM
+#define CH2_PWM  OCR0B      // OCR0B is the output compare register for PB1
+
+#define AUXLED_PIN   PB4    // pin 3
+
+// e-switch
+#ifndef SWITCH_PIN
+#define SWITCH_PIN   PB3    // pin 2
+#define SWITCH_PCINT PCINT3 // pin 2 pin change interrupt
+#endif
+
+#define USE_VOLTAGE_DIVIDER  // use a voltage divider on pin 7, not VCC
+#ifndef VOLTAGE_PIN
+#define VOLTAGE_PIN      PB2    // pin 7, voltage ADC
+#define VOLTAGE_CHANNEL  0x01   // MUX 01 corresponds with PB2
+#define VOLTAGE_ADC      ADC1D  // Digital input disable bit corresponding with PB2
+// inherited from tk-attiny.h
+//#define VOLTAGE_ADC_DIDR DIDR0  // DIDR for ADC1
+// 1.1V reference, left-adjust, ADC1/PB2
+//#define ADMUX_VOLTAGE_DIVIDER ((1 << V_REF) | (1 << ADLAR) | VOLTAGE_CHANNEL)
+// 1.1V reference, no left-adjust, ADC1/PB2
+#define ADMUX_VOLTAGE_DIVIDER ((1 << V_REF) | VOLTAGE_CHANNEL)
+#endif
+#define ADC_PRSCL   0x07    // clk/128
+
+// Raw ADC readings at 4.4V and 2.2V (in-between, we assume values form a straight line)
+#ifndef ADC_44
+#define ADC_44 (234*4)
+#endif
+#ifndef ADC_22
+#define ADC_22 (117*4)
+#endif
+
+#define FAST 0xA3           // fast PWM both channels
+#define PHASE 0xA1          // phase-correct PWM both channels
+
+
+inline void hwdef_setup() {
+    // configure PWM channels
+    DDRB = (1 << CH1_PIN)
+         | (1 << CH2_PIN);
+
+    TCCR0B = 0x01; // pre-scaler for timer (1 => 1, 2 => 8, 3 => 64...)
+    TCCR0A = PHASE;
+
+    // configure e-switch
+    PORTB = (1 << SWITCH_PIN);  // e-switch is the only input
+    PCMSK = (1 << SWITCH_PIN);  // pin change interrupt uses this pin
+}
+
+
+#define LAYOUT_DEFINED
+
--- spaghetti-monster/anduril/cfg-mateminco-mf01s.h
+++ spaghetti-monster/anduril/cfg-mateminco-mf01s.h
@@ -1,10 +1,11 @@
-// Mateminco/Astrolux MF01S options for Anduril
+// Mateminco MT18S / Astrolux MF01S options for Anduril
 // Copyright (C) 2019-2023 Selene ToyKeeper
 // SPDX-License-Identifier: GPL-3.0-or-later
 #pragma once
 
 #define MODEL_NUMBER "0511"
-#include "hwdef-Mateminco_MF01S.h"
+#include "hwdef-mateminco-mf01s.h"
+// ATTINY: 85
 
 // the button lights up
 #define USE_INDICATOR_LED
@@ -15,10 +16,7 @@
 #define INDICATOR_LED_DEFAULT_MODE ((3<<2) + 1)
 
 
-// don't blink during ramp, it's irrelevant and annoying on this light
-#define BLINK_AT_RAMP_CEIL
-#undef BLINK_AT_RAMP_MIDDLE
-#undef BLINK_AT_RAMP_FLOOR
+#define RAMP_SIZE 150
 
 // measured brightness with 4x30Q cells at 4.11V:
 // moon: 2.5 lm
@@ -26,9 +24,9 @@
 // channel 2: 13500 lm
 // ../../../bin/level_calc.py seventh 2 150 7135 1 12 717 FET 1 10 13000
 // (with some manual tweaks afterward)
-#define RAMP_LENGTH 150
 #define PWM1_LEVELS 1,1,2,2,3,3,4,4,5,5,6,7,7,8,9,10,11,12,13,14,15,17,18,19,21,22,24,26,28,30,32,34,36,38,41,44,46,49,52,55,59,62,66,70,74,78,83,87,92,97,102,108,114,120,126,133,139,147,154,162,170,178,187,196,206,215,226,236,248,255,235,255,240,255,245,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,0
 #define PWM2_LEVELS 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,2,2,3,3,4,4,5,6,7,8,9,10,11,12,13,14,16,17,19,20,22,23,25,26,28,30,31,33,35,37,39,41,43,45,47,49,52,54,57,59,62,65,67,70,73,76,80,83,86,90,93,97,101,105,109,113,117,122,126,131,135,140,145,151,156,161,167,173,179,185,191,197,204,211,218,225,232,239,247,255
+
 #define MAX_1x7135 70  // ~626 lm
 #define HALFSPEED_LEVEL 23
 #define QUARTERSPEED_LEVEL 6
@@ -49,5 +47,15 @@
 #define THERM_FASTER_LEVEL 125  // throttle back faster when high (>6000 lm)
 #define THERM_HARD_TURBO_DROP  // this light is massively overpowered
 
+// don't blink during ramp, it's irrelevant and annoying on this light
+#define BLINK_AT_RAMP_CEIL
+#undef BLINK_AT_RAMP_MIDDLE
+#undef BLINK_AT_RAMP_FLOOR
+
+// enable extra features
+#define USE_SMOOTH_STEPS
+
 // too big, turn off extra features
 #undef USE_TACTICAL_MODE
+#undef USE_SOS_MODE
+
