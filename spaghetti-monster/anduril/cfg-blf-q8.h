b096e3c4dc3279df35ffd2a742ec8776834207dc
Selene ToyKeeper <git@toykeeper.net>
2023-10-27 05:58:27 -0600

    enabled smooth steps on blf-q8 and sofirn-sp36, instead of tactical mode

--- spaghetti-monster/anduril/cfg-blf-q8.h
+++ spaghetti-monster/anduril/cfg-blf-q8.h
@@ -59,6 +59,8 @@
 #undef BLINK_AT_RAMP_MIDDLE
 #endif
 
+#define USE_SMOOTH_STEPS
+
 // too big, remove stuff to make room
 #undef USE_SOS_MODE
 //#undef USE_RAMP_AFTER_MOON_CONFIG
@@ -66,4 +68,4 @@
 //#undef USE_VOLTAGE_CORRECTION
 //#undef USE_2C_STYLE_CONFIG
 //#undef USE_TACTICAL_STROBE_MODE
-//#undef USE_TACTICAL_MODE
+#undef USE_TACTICAL_MODE
