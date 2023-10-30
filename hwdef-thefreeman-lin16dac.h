1e60feec749739ee917e4c9dc8b144b3ac1946be
Selene ToyKeeper <git@toykeeper.net>
2023-10-30 10:12:50 -0600

    converted thefreeman-lin16dac to new API, but unsure if it works since I it's very uncommon hardware I don't have (also, there are some obvious things needing fixing, but I'd need hardware  to calibrate it correctly)

--- hwdef-thefreeman-lin16dac.h
+++ hwdef-thefreeman-lin16dac.h
@@ -1,5 +1,5 @@
 // thefreeman's Linear 16 driver using DAC control
-// Copyright (C) 2021-2023 (FIXME)
+// Copyright (C) 2021-2023 thefreeman, Selene ToyKeeper
 // SPDX-License-Identifier: GPL-3.0-or-later
 #pragma once
 
@@ -12,41 +12,68 @@
  * Read voltage from VCC pin, has PFET so no drop
  */
 
-#define LAYOUT_DEFINED
-
-#ifdef ATTINY
-#undef ATTINY
-#endif
 #define ATTINY 1616
 #include <avr/io.h>
 
-#define PWM_CHANNELS 1
-#define USE_DYN_PWM  // dynamic frequency and speed
+#define HWDEF_C_FILE hwdef-thefreeman-lin16dac.c
 
-#ifndef SWITCH_PIN
-#define SWITCH_PIN     PIN4_bp
-#define SWITCH_PORT    VPORTB.IN
-#define SWITCH_ISC_REG PORTB.PIN2CTRL
-#define SWITCH_VECT    PORTB_PORT_vect
-#define SWITCH_INTFLG  VPORTB.INTFLAGS
-#endif
+// allow using aux LEDs as extra channel modes
+#include "chan-aux.h"
+
+// channel modes:
+// * 0. main LEDs
+// * 1+. aux RGB
+#define NUM_CHANNEL_MODES  2
+enum CHANNEL_MODES {
+    CM_MAIN = 0,
+    CM_AUX
+};
+
+#define DEFAULT_CHANNEL_MODE  CM_MAIN
+
+// right-most bit first, modes are in fedcba9876543210 order
+#define CHANNEL_MODES_ENABLED 0b0000000000000001
 
 
-#define PWM1_LVL DAC0.DATA  // use this for DAC voltage output
+#define PWM_CHANNELS  1  // old, remove this
 
-// PWM parameters of both channels are tied together because they share a counter
-#define PWM1_TOP VREF.CTRLA   // holds the TOP value for for variable-resolution PWM
+#define PWM_BITS      8         // 8-bit DAC
+#define PWM_GET       PWM_GET8
+#define PWM_DATATYPE  uint8_t
+#define PWM_DATATYPE2 uint16_t  // only needs 32-bit if ramp values go over 255
+#define PWM1_DATATYPE uint8_t   // main LED ramp
 
-// For enabling / disabling the HDR high-range channel
-#define LED_ENABLE_PIN   PIN3_bp
-#define LED_ENABLE_PORT  PORTB_OUT
-#define LED_ENABLE_PIN_LEVEL_MIN 35
-#define LED_ENABLE_PIN_LEVEL_MAX 150
+// main LED outputs
+#define DAC_LVL   DAC0.DATA    // 0 to 255, for 0V to Vref
+#define DAC_VREF  VREF.CTRLA   // 0.55V or 2.5V
+#define PWM_TOP_INIT  255      // highest value used in top half of ramp (unused?)
+// Vref values
+#define V055  16
+#define V11   17
+#define V25   18
+#define V43   19
+#define V15   20
 
+// Opamp enable
 // For turning on and off the op-amp
-#define LED2_ENABLE_PIN   PIN7_bp
-#define LED2_ENABLE_PORT  PORTA_OUT
-#define LED2_ON_DELAY 80  // how many ms to delay turning on the lights after enabling the channel
+#define OPAMP_ENABLE_PIN   PIN7_bp
+#define OPAMP_ENABLE_PORT  PORTA_OUT
+// how many ms to delay turning on the lights after enabling the channel
+// (FIXME: 80 is long enough it's likely to cause bugs elsewhere,
+//  as events stack up unhandled for 5 consecutive WDT ticks)
+#define OPAMP_ON_DELAY 80
+
+// HDR
+// turns on HDR FET for the high current range
+#define HDR_ENABLE_PIN   PIN3_bp
+#define HDR_ENABLE_PORT  PORTB_OUT
+
+// e-switch
+#define SWITCH_PIN      PIN4_bp
+#define SWITCH_PORT     VPORTB.IN
+#define SWITCH_ISC_REG  PORTB.PIN2CTRL
+#define SWITCH_VECT     PORTB_PORT_vect
+#define SWITCH_INTFLG   VPORTB.INTFLAGS
 
 // average drop across diode on this hardware
 #ifndef VOLTAGE_FUDGE_FACTOR
@@ -54,22 +81,21 @@
 #endif
 
 // lighted button
-#ifndef AUXLED_PIN
-#define AUXLED_PIN  PIN5_bp
-#define AUXLED_PORT PORTB
-#endif
+#define AUXLED_PIN   PIN5_bp
+#define AUXLED_PORT  PORTB
 
 
-// with so many pins, doing this all with #ifdefs gets awkward...
-// ... so just hardcode it in each hwdef file instead
 inline void hwdef_setup() {
 
     // set up the system clock to run at 10 MHz instead of the default 3.33 MHz
-    // TODO: for this DAC controlled-light, try to decrease the clock speed or use the ULP
-    _PROTECTED_WRITE( CLKCTRL.MCLKCTRLB, CLKCTRL_PDIV_2X_gc | CLKCTRL_PEN_bm );
+    // (it'll get underclocked to 2.5 MHz later)
+    // TODO: maybe run even slower?
+    _PROTECTED_WRITE( CLKCTRL.MCLKCTRLB,
+                      CLKCTRL_PDIV_2X_gc | CLKCTRL_PEN_bm );
 
-    VPORTA.DIR = PIN6_bm | PIN7_bm;
-    VPORTB.DIR = PIN3_bm;
+    VPORTA.DIR = PIN6_bm   // DAC
+               | PIN7_bm;  // Opamp
+    VPORTB.DIR = PIN3_bm;  // HDR
     //VPORTC.DIR = 0b00000000;
 
     // enable pullups on the input pins to reduce power
@@ -84,9 +110,10 @@ inline void hwdef_setup() {
 
     PORTB.PIN0CTRL = PORT_PULLUPEN_bm;
     PORTB.PIN1CTRL = PORT_PULLUPEN_bm;
-    PORTB.PIN2CTRL = PORT_PULLUPEN_bm; 
+    PORTB.PIN2CTRL = PORT_PULLUPEN_bm;
     //PORTB.PIN3CTRL = PORT_PULLUPEN_bm;  // HDR channel selection
-    PORTB.PIN4CTRL = PORT_PULLUPEN_bm | PORT_ISC_BOTHEDGES_gc;  // switch
+    PORTB.PIN4CTRL = PORT_PULLUPEN_bm
+                   | PORT_ISC_BOTHEDGES_gc;  // e-switch
     //PORTB.PIN5CTRL = PORT_PULLUPEN_bm;  // Aux LED
 
     PORTC.PIN0CTRL = PORT_PULLUPEN_bm;
@@ -97,10 +124,14 @@ inline void hwdef_setup() {
     // set up the DAC
     // https://ww1.microchip.com/downloads/en/DeviceDoc/ATtiny1614-16-17-DataSheet-DS40002204A.pdf
     // DAC ranges from 0V to (255 * Vref) / 256
-    VREF.CTRLA |= VREF_DAC0REFSEL_2V5_gc; // also VREF_DAC0REFSEL_0V55_gc and VREF_DAC0REFSEL_1V1_gc and VREF_DAC0REFSEL_2V5_gc 
+    // also VREF_DAC0REFSEL_0V55_gc and VREF_DAC0REFSEL_1V1_gc and VREF_DAC0REFSEL_2V5_gc
+    VREF.CTRLA |= VREF_DAC0REFSEL_2V5_gc;
     VREF.CTRLB |= VREF_DAC0REFEN_bm;
     DAC0.CTRLA = DAC_ENABLE_bm | DAC_OUTEN_bm;
     DAC0.DATA = 255; // set the output voltage
 
 }
+
+
+#define LAYOUT_DEFINED
 
