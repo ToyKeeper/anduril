a4d96f08a017ce9d475b2575c597e741c6e8af1e
Selene ToyKeeper <git@toykeeper.net>
2023-11-19 01:42:33 -0700

    version check: allow hex digits

--- ui/anduril/version-check-mode.c
+++ ui/anduril/version-check-mode.c
@@ -15,7 +15,10 @@ uint8_t version_check_state(Event event, uint16_t arg) {
 inline void version_check_iter() {
     for (uint8_t i=0; i<sizeof(version_number)-1; i++) {
         uint8_t digit = pgm_read_byte(version_number + i) - '0';
-        if (digit < 10) blink_digit(digit);
+        //       digits: 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+        //   hex digits: 0 1 2 3 4 5 6 7 8 9  a  b  c  d  e  f
+        // 'model' file: 0 1 2 3 4 5 6 7 8 9  :  ;  <  =  >  ?
+        if (digit < 16) blink_digit(digit);
         else {  // "buzz" for non-numeric characters
             for(uint8_t frame=0; frame<25; frame++) {
                 set_level((frame&1) << 5);
