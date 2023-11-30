fdac6fcf22cd658785480c88c28243eeae62bcd0
Selene ToyKeeper <git@toykeeper.net>
2023-11-30 09:16:13 -0700

    made the avr32dd20 flashing script more universal

--- bin/flash-avr32dd20.sh
+++ bin/flash-avr32dd20.sh
@@ -1,13 +1,40 @@
 #!/usr/bin/env bash
+# AVR firmware flashing script, a wrapper for other tools
+# Copyright (C) 2023 Selene ToyKeeper
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# Usage: flash-MCUTYPE.sh hex/foo.hex
+# (where the script name specifies the type of MCU chip,
+#  and the first parameter is the path to a .hex file)
+# Example:
+#   ./bin/flash-avr32dd20.sh hex/anduril.thefreeman-avr32dd20-devkit.hex
 
 set -e
 
+# Get the path to a .hex file
 [[ -z "$1" ]] && echo "No .hex file specified." && exit 1
 HEX="$1"
 
-#pymcuprog ping -t uart -u /dev/ttyUSB2 -d avr32dd20
-#pymcuprog erase -t uart -u /dev/ttyUSB2 -d avr32dd20
-#pymcuprog write verify -t uart -u /dev/ttyUSB2 -d avr32dd20 -f "$HEX"
-#pymcuprog verify -t uart -u /dev/ttyUSB2 -d avr32dd20 -f "$HEX"
+# assume the highest-numbered USB serial device is the correct one,
+# since it was probably the most recently plugged in
+TTYUSB=$(ls -1 /dev/tty* | grep -i usb | tail -1)
 
-pymcuprog write --erase --verify --timing -t uart -u /dev/ttyUSB2 -d avr32dd20 -f "$HEX"
+# figure out the MCU type...
+# TODO: find the relevant hw/*/arch file and use that to get MCU type
+# use $2 if it exists, and use the name of this script maybe
+MCUTYPE="unset"
+[[ -n "$2" ]] && MCUTYPE="$2"
+#MCUTYPE=$(echo "$0" | sed 's/.*flash-\(.*\).sh.*/\1/')
+[[ "$0" =~ flash-(.*).sh ]] && MCUTYPE="${BASH_REMATCH[1]}"
+
+# Do the actual flashing
+echo "Flashing $MCUTYPE MCU on port $TTYUSB: $HEX"
+echo pymcuprog write \
+    --erase \
+    --verify \
+    --timing \
+    -t uart \
+    -u "$TTYUSB" \
+    -d "$MCUTYPE" \
+    -f "$HEX"
+
