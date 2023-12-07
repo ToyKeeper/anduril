7f9f0f13489ff19bdc499bc825db51b78e7f6e58
Selene ToyKeeper <git@toykeeper.net>
2023-12-06 19:07:31 -0700

    added wurkkos-ts25-boost (early version made from spec, no clue if it works)

---
+++ hw/wurkkos/ts25/boost/anduril.h
@@ -0,0 +1,79 @@
+// Wurkkos TS25 Boost
+// Copyright (C) 2023 Selene ToyKeeper
+// SPDX-License-Identifier: GPL-3.0-or-later
+#pragma once
+
+#define HWDEF_H  wurkkos/ts25/boost/hwdef.h
+#include "wurkkos/anduril.h"
+
+// this light has three aux LED channels: R, G, B
+#define USE_AUX_RGB_LEDS
+
+// don't turn on the aux LEDs while main LEDs are on
+#ifdef USE_INDICATOR_LED_WHILE_RAMPING
+#undef USE_INDICATOR_LED_WHILE_RAMPING
+#endif
+
+#define VOLTAGE_FUDGE_FACTOR 5  // add 0.25V, not 0.35V
+
+#define RAMP_SIZE 150
+
+// delta-sigma modulated PWM (0b0HHHHHHHHLLLLLLL = 0, 8xHigh, 7xLow bits)
+// level_calc.py 5.01 1 150 7135 0 0.2 2000 --pwm 32640
+// (max is (255 << 7), because it's 8-bit PWM plus 7 bits of DSM)
+#define PWM1_LEVELS 0,1,2,3,4,5,6,7,9,10,12,14,17,19,22,25,28,32,36,41,45,50,56,62,69,76,84,92,101,110,121,132,143,156,169,184,199,215,232,251,270,291,313,336,360,386,414,442,473,505,539,574,612,651,693,736,782,829,880,932,987,1045,1105,1168,1233,1302,1374,1449,1527,1608,1693,1781,1873,1969,2068,2172,2279,2391,2507,2628,2753,2883,3018,3158,3303,3454,3609,3771,3938,4111,4289,4475,4666,4864,5068,5280,5498,5724,5957,6197,6445,6701,6965,7237,7518,7808,8106,8413,8730,9056,9392,9737,10093,10459,10835,11223,11621,12031,12452,12884,13329,13786,14255,14737,15232,15741,16262,16798,17347,17911,18489,19082,19691,20314,20954,21609,22281,22969,23674,24397,25137,25895,26671,27465,28279,29111,29963,30835,31727,32640
+#define MIN_THERM_STEPDOWN 50
+#define DEFAULT_LEVEL 70
+#define MAX_1x7135 150
+// moon at 1/4th speed for max runtime
+// the rest at half speed, to reduce ripple
+// but not full speed because 8 kHz PWM is enough for this circuit
+// and speed changes make a big visible bump
+#define HALFSPEED_LEVEL      255
+#define QUARTERSPEED_LEVEL   2
+
+#define RAMP_SMOOTH_FLOOR    1
+#define RAMP_SMOOTH_CEIL     150
+// 10, 30, 50, [70], 90, 110, 130
+#define RAMP_DISCRETE_FLOOR  10
+#define RAMP_DISCRETE_CEIL   130
+#define RAMP_DISCRETE_STEPS  7
+
+// at Wurkkos's request, reduce the Simple UI ceiling a little bit
+// 25 50 [75] 100 125
+#define SIMPLE_UI_FLOOR  25
+#define SIMPLE_UI_CEIL   125
+#define SIMPLE_UI_STEPS  5
+
+// stop panicking at ~50% power
+#define THERM_FASTER_LEVEL 120  // throttle back faster when high
+
+#define BLINK_BRIGHTNESS 50
+#define BLINK_ONCE_TIME 12
+
+// show each channel while it scroll by in the menu
+#define USE_CONFIG_COLORS
+
+// blink numbers on the main LEDs by default
+// (so battcheck will be visible while charging)
+#define DEFAULT_BLINK_CHANNEL  CM_MAIN
+// blink numbers on the aux LEDs by default
+//#define DEFAULT_BLINK_CHANNEL  CM_AUXWHT
+
+// slow down party strobe; this driver can't pulse for 2ms or less
+#define PARTY_STROBE_ONTIME 3
+
+// use aux red + aux blue for police strobe
+#define USE_POLICE_COLOR_STROBE_MODE
+#define POLICE_STROBE_USES_AUX
+#define POLICE_COLOR_STROBE_CH1        CM_AUXRED
+#define POLICE_COLOR_STROBE_CH2        CM_AUXBLU
+
+// the default of 26 looks a bit rough, so increase it to make it smoother
+#define CANDLE_AMPLITUDE 33
+
+// don't blink mid-ramp
+#ifdef BLINK_AT_RAMP_MIDDLE
+#undef BLINK_AT_RAMP_MIDDLE
+#endif
+
---
+++ hw/wurkkos/ts25/boost/hwdef.h
@@ -0,0 +1,174 @@
+// Wurkkos TS25 driver layout
+// Copyright (C) 2023 Selene ToyKeeper
+// SPDX-License-Identifier: GPL-3.0-or-later
+#pragma once
+
+/* Boost driver based on MP3432 and attiny1616
+ * with high dynamic range and DAC control, AUX : RGB + button
+ * hardware version : 2.0+
+ * compatible with BST20-FWxA v1.0 (no button LED A)
+ *
+ * Pin / Name / Function
+ *   1    PA2   -
+ *   2    PA3   -
+ *   3    GND   GND
+ *   4    VCC   VCC
+ *   5    PA4   -
+ *   6    PA5   e-switch
+ *   7    PA6   -
+ *   8    PA7   -
+ *   9    PB5   -
+ *  10    PB4   -
+ *  11    PB3   -
+ *  12    PB2   EN: boost enable
+ *  13    PB1   -
+ *  14    PB0   PWM
+ *  15    PC0   -
+ *  16    PC1   B: aux blue
+ *  17    PC2   G: aux green
+ *  18    PC3   R: aux red
+ *  19    PA0   UDPI
+ *  20    PA1   Vbat sense (voltage divider)
+ *
+ */
+
+#define HWDEF_C  wurkkos/ts25/boost/hwdef.c
+
+// allow using aux LEDs as extra channel modes
+#include "fsm/chan-rgbaux.h"
+
+// channel modes:
+// * 0. main LEDs
+// * 1+. aux RGB
+#define NUM_CHANNEL_MODES   (1 + NUM_RGB_AUX_CHANNEL_MODES)
+enum CHANNEL_MODES {
+    CM_MAIN = 0,
+    RGB_AUX_ENUMS
+};
+
+#define DEFAULT_CHANNEL_MODE  CM_MAIN
+
+// right-most bit first, modes are in fedcba9876543210 order
+#define CHANNEL_MODES_ENABLED 0b0000000000000001
+
+
+#define PWM_BITS      16        // 0 to 32640 (0 to 255 PWM + 0 to 127 DSM) at constant kHz
+#define PWM_GET       PWM_GET16
+#define PWM_DATATYPE  uint16_t
+#define PWM_DATATYPE2 uint32_t  // only needs 32-bit if ramp values go over 255
+#define PWM1_DATATYPE uint16_t  // 15-bit PWM+DSM ramp
+
+#define PWM_TOP  TCA0.SINGLE.PERBUF  // holds the TOP value for for variable-resolution PWM
+#define PWM_TOP_INIT  255
+#define PWM_CNT  TCA0.SINGLE.CNT  // for checking / resetting phase
+// (max is (255 << 7), because it's 8-bit PWM plus 7 bits of DSM)
+#define DSM_TOP       (255<<7) // 15-bit resolution leaves 1 bit for carry
+
+// timer interrupt for DSM
+#define DSM_vect     TCA0_OVF_vect
+#define DSM_INTCTRL  TCA0.SINGLE.INTCTRL
+#define DSM_INTFLAGS TCA0.SINGLE.INTFLAGS
+#define DSM_OVF_bm   TCA_SINGLE_OVF_bm
+
+#define DELAY_FACTOR 90  // less time in delay() because more time spent in interrupts
+
+// regulated channel
+uint16_t ch1_dsm_lvl;
+uint8_t ch1_pwm, ch1_dsm;
+#define CH1_PIN  PB0
+#define CH1_PWM  TCA0.SINGLE.CMP0BUF  // CMP0 is the output compare register for PB0
+
+#define CH1_ENABLE_PIN   PIN2_bp
+#define CH1_ENABLE_PORT  PORTB_OUT
+
+// e-switch
+#define SWITCH_PIN      PIN5_bp
+#define SWITCH_PORT     VPORTA.IN
+#define SWITCH_ISC_REG  PORTA.PIN2CTRL
+#define SWITCH_VECT     PORTA_PORT_vect
+#define SWITCH_INTFLG   VPORTA.INTFLAGS
+
+// Voltage divider battLVL
+#define USE_VOLTAGE_DIVIDER       // use a dedicated pin, not VCC, because VCC input is regulated
+#define ADMUX_VOLTAGE_DIVIDER  ADC_MUXPOS_AIN1_gc  // which ADC channel to read
+// don't use the default VDD converter
+#undef voltage_raw2cooked
+#define voltage_raw2cooked  mcu_vdivider_raw2cooked
+
+// Raw ADC readings at 4.4V and 2.2V
+// calibrate the voltage readout here
+// estimated / calculated values are:
+//   (voltage - D1) * (R2/(R2+R1) * 4096 / 1.1)
+// Resistors are 330k and 100k
+#define ADC_44  3810  // raw value at 4.40V
+#define ADC_22  1905  // raw value at 2.20V
+
+// this light has aux LEDs under the optic
+#define AUXLED_R_PIN    PIN3_bp    // pin 3
+#define AUXLED_G_PIN    PIN2_bp    // pin 2
+#define AUXLED_B_PIN    PIN1_bp    // pin 1
+#define AUXLED_RGB_PORT PORTC  // PORTA or PORTB or PORTC
+
+// this light has three aux LED channels: R, G, B
+#define USE_AUX_RGB_LEDS
+
+
+inline void hwdef_setup() {
+
+    mcu_clock_speed();
+
+    // Outputs:
+    //VPORTA.DIR = ...;
+    VPORTB.DIR = PIN0_bm   // PWM
+               | PIN2_bm;  // boost enable
+    VPORTC.DIR = PIN1_bm   // aux B
+               | PIN2_bm   // aux G
+               | PIN3_bm;  // aux R
+
+    // enable pullups on the unused pins to reduce power
+    PORTA.PIN0CTRL = PORT_PULLUPEN_bm;
+    //PORTA.PIN1CTRL = PORT_PULLUPEN_bm;  // Vbat sense
+    PORTA.PIN2CTRL = PORT_PULLUPEN_bm;
+    PORTA.PIN3CTRL = PORT_PULLUPEN_bm;
+    PORTA.PIN4CTRL = PORT_PULLUPEN_bm;
+    PORTA.PIN5CTRL = PORT_PULLUPEN_bm | PORT_ISC_BOTHEDGES_gc;  // eSwitch
+    PORTA.PIN6CTRL = PORT_PULLUPEN_bm;
+    PORTA.PIN7CTRL = PORT_PULLUPEN_bm;
+
+    //PORTB.PIN0CTRL = PORT_PULLUPEN_bm;  // regulated channel
+    PORTB.PIN1CTRL = PORT_PULLUPEN_bm;
+    //PORTB.PIN2CTRL = PORT_PULLUPEN_bm;  // boost enable
+    PORTB.PIN3CTRL = PORT_PULLUPEN_bm;
+    PORTB.PIN4CTRL = PORT_PULLUPEN_bm;
+    PORTB.PIN5CTRL = PORT_PULLUPEN_bm;
+
+    PORTC.PIN0CTRL = PORT_PULLUPEN_bm;
+    //PORTC.PIN1CTRL = PORT_PULLUPEN_bm;  // RGB Aux
+    //PORTC.PIN2CTRL = PORT_PULLUPEN_bm;  // RGB Aux
+    //PORTC.PIN3CTRL = PORT_PULLUPEN_bm;  // RGB Aux
+
+    // set up the PWM
+    // https://ww1.microchip.com/downloads/en/DeviceDoc/ATtiny1614-16-17-DataSheet-DS40002204A.pdf
+    // PB0 is TCA0:WO0, use TCA_SINGLE_CMP0EN_bm
+    // PB1 is TCA0:WO1, use TCA_SINGLE_CMP1EN_bm
+    // PB2 is TCA0:WO2, use TCA_SINGLE_CMP2EN_bm
+    // For Fast (Single Slope) PWM use TCA_SINGLE_WGMODE_SINGLESLOPE_gc
+    // For Phase Correct (Dual Slope) PWM use TCA_SINGLE_WGMODE_DSBOTTOM_gc
+    // See the manual for other pins, clocks, configs, portmux, etc
+    TCA0.SINGLE.CTRLB = TCA_SINGLE_CMP0EN_bm
+                      //| TCA_SINGLE_CMP1EN_bm
+                      | TCA_SINGLE_WGMODE_DSBOTTOM_gc;
+    TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV1_gc
+                      | TCA_SINGLE_ENABLE_bm;
+
+    PWM_TOP = PWM_TOP_INIT;
+
+    // set up interrupt for delta-sigma modulation
+    // (moved to hwdef.c functions so it can be enabled/disabled based on ramp level)
+    //DSM_INTCTRL |= DSM_OVF_bm;  // interrupt once for each timer cycle
+
+}
+
+
+#define LAYOUT_DEFINED
+
