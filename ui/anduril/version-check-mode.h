bde3eca2af693c67f7c4e907081907c891dd0a7d
gchart <gabe@ghart.com>
2025-12-11 07:18:07 -0600

    Add Sofirn SC13 and catch up to TK

--- ui/anduril/version-check-mode.h
+++ ui/anduril/version-check-mode.h
@@ -12,6 +12,10 @@
 #error MODEL_NUMBER undefined
 #endif
 
+#ifndef BUZZ_SPEED
+#define BUZZ_SPEED 16
+#endif
+
 #include "anduril/version.h"
 const PROGMEM uint8_t version_number[] = MODEL_NUMBER "." VERSION_NUMBER;
 uint8_t version_check_state(Event event, uint16_t arg);
