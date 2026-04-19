a6ba176e01f8cd56a5c0cdc08c5e4b2dd5e7998b
Whezzel <whezzel@gmail.com>
2026-04-19 14:46:24 -0500

    refactor(whezzel): remove manual memory timer and autolock overrides

--- hw/whezzel/emisar-d3aa/anduril.h
+++ hw/whezzel/emisar-d3aa/anduril.h
@@ -1,8 +1,3 @@
 #pragma once
 #include "hank/emisar-d3aa/anduril.h"
 #include "whezzel/anduril.h"
-
-//#ifdef DEFAULT_AUTOLOCK_TIME
-//    #undef DEFAULT_AUTOLOCK_TIME
-//#endif
-//#define DEFAULT_AUTOLOCK_TIME 0
--- hw/whezzel/lumintop-fw3a/anduril.h
+++ hw/whezzel/lumintop-fw3a/anduril.h
@@ -1,8 +1,3 @@
 #pragma once
 #include "lumintop/fw3a/anduril.h"
 #include "whezzel/anduril.h"
-
-#ifdef DEFAULT_AUTOLOCK_TIME
-    #undef DEFAULT_AUTOLOCK_TIME
-#endif
-#define DEFAULT_AUTOLOCK_TIME 0
