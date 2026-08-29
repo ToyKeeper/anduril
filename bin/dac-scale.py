28f3e218cef3c02f16b6bebf8369f2ca83624fb2
Selene ToyKeeper <git@toykeeper.net>
2026-08-29 00:45:33 -0600

    bin/dac-scale.py: made it work for attiny1616 too, not just avr32dd
    If the maximum DAC value is 255 or lower, it uses attiny1616 defaults.
    Otherwise, it uses avr32dd defaults.
    
    Had to redo a lot of the math.  Values may be slightly different now.
    I had a lot of the math wrong before, because the DAC limit (1023)
    happened to be almost identical to the low Vref (int(1.024 * 1000)),
    and this was not true on t1616 (255 != 550).
    
    Anyway, it works for both now.  Still just 2 "gears" though.
    For the lowest "gear" on Lume-X drivers, do the lowest one manually.

--- bin/dac-scale.py
+++ bin/dac-scale.py
@@ -1,19 +1,25 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 #
 # Calculates DAC values and Vref values from a list of raw intensity values
 # Usage: dac-scale.py 5,11,18,25,...,370474,384985,400000
 # Output: #defines suitable for use in hw/*/anduril.h
 # Assumptions:
-# - DAC data range is 0 to 1023
-# - DAC Vrefs are 1.024V and 2.5V
-# - HDR is available and has 2 steps
-# - HDR channel ratio is "highest_ramp_value / 2500"
+# - If DAC max is 1023:
+#   - DAC data range is 0 to 1023
+#   - DAC Vrefs are 1.024V and 2.5V
+#   - HDR is available and has 2 steps
+#   - HDR channel ratio is "highest_ramp_value / 2500"
+# - If DAC max is 255:
+#   - DAC data range is 0 to 255
+#   - DAC Vrefs are 0.55V and 2.5V
+#   - HDR is available and has 2 steps
+#   - HDR channel ratio is "highest_ramp_value / 2500"
 #
 # Output values thus have 4 "engine gears":
-# - HDR low , 1.024V Vref
+# - HDR low , 1.024V Vref  (or 0.55V)
 # - HDR low , 2.5  V Vref
-# - HDR high, 1.024V Vref
+# - HDR high, 1.024V Vref  (or 0.55V)
 # - HDR high, 2.5  V Vref
 #
 
@@ -28,9 +34,22 @@ def main(args):
         mcu_max_pwm = int(args[0])
         del args[0]
 
+    if mcu_max_pwm < 256:
+        low_vref_name = 'V05'
+        low_vref_float = 0.55
+        low_vref_int = 550
+    else:
+        low_vref_name = 'V10'
+        low_vref_float = 1.024
+        low_vref_int = 1024
+
+    hi_vref_name = 'V25'
+    hi_vref_float = 2.5
+    hi_vref_int = 2500
+
     # get desired brightness levels and set up vars
     raw_pwm = [int(x) for x in args[0].split(',')]
-    ratio = raw_pwm[-1] / 2500.0
+    ratio = raw_pwm[-1] / float(hi_vref_int)
     cooked = [[]]
 
     def limit(p):
@@ -44,26 +63,33 @@ def main(args):
         # limit maximum power maybe
         if max_pwm != mcu_max_pwm:
             raw = raw * max_pwm / mcu_max_pwm
+        # low gear, low vref
         if 0 == phase:
-            if raw <= mcu_max_pwm:
-                cooked[-1].append(limit(raw))
+            if raw <= low_vref_int:
+                cooked[-1].append(limit(raw * mcu_max_pwm / low_vref_int))
             else:
                 phase += 1
                 cooked.append([])
+        # low gear, high vref
         if 1 == phase:
-            if raw <= int(2500):
-                cooked[-1].append(limit(raw * 1.024 / 2.5))
+            if raw <= hi_vref_int:
+                cooked[-1].append(limit(raw * mcu_max_pwm / hi_vref_int))
             else:
                 phase += 1
                 cooked.append([])
+        # high gear, low vref
         if 2 == phase:
-            if raw <= int(1024 * ratio):
-                cooked[-1].append(limit(raw / ratio))
+            if raw <= int(low_vref_int * ratio):
+            #if raw <= int(mcu_max_pwm * ratio):
+                cooked[-1].append(limit(raw * mcu_max_pwm / low_vref_int / ratio))
             else:
                 phase += 1
                 cooked.append([])
+        # high gear, high vref
         if 3 == phase:
-            cooked[-1].append(turbo_limit(raw * 1.024 / 2.5 / ratio))
+            cooked[-1].append(turbo_limit(raw * mcu_max_pwm / hi_vref_int / ratio))
+
+        #print(f'{phase}: {raw}: {cooked[-1][-1]}')
 
     # "gear change" boundaries
     b1 =      len(cooked[0])
@@ -89,10 +115,10 @@ def main(args):
     lines.append('        ' + fmt_pwms(cooked[2]) + ', \\')
     lines.append('        ' + fmt_pwms(cooked[3]))
     lines.append('#define PWM2_LEVELS \\')
-    lines.append('        ' + fmt_tops('V10', cooked[0]) + ', \\')
-    lines.append('        ' + fmt_tops('V25', cooked[1]) + ', \\')
-    lines.append('        ' + fmt_tops('V10', cooked[2]) + ', \\')
-    lines.append('        ' + fmt_tops('V25', cooked[3]))
+    lines.append('        ' + fmt_tops(low_vref_name, cooked[0]) + ', \\')
+    lines.append('        ' + fmt_tops('V25',         cooked[1]) + ', \\')
+    lines.append('        ' + fmt_tops(low_vref_name, cooked[2]) + ', \\')
+    lines.append('        ' + fmt_tops('V25',         cooked[3]))
 
     lines.append('#define MAX_1x7135           %3i' % b2)
     lines.append('#define HDR_ENABLE_LEVEL_MIN %3i' % (b2+1))
