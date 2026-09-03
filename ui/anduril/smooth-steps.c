7b0010bf7a7e1941f20cff0c363d669dee0cc6b7
Selene ToyKeeper <git@toykeeper.net>
2026-09-03 03:37:01 -0600

    fixed bug: smooth beacon got stuck on d3aa/kr1aa with weak battery, if target level was above the weak battery limit level
    (it would never reach the target level,
     so the ramp-up animation would never end)
    
    This was probably happening before, too, in the regular ramp mode...
    ... but it wouldn't be noticeable there
    since it would have no visible symptoms.

--- ui/anduril/smooth-steps.c
+++ ui/anduril/smooth-steps.c
@@ -37,11 +37,18 @@ void smooth_steps_iter() {
 }
 
 void set_level_smooth(uint8_t level, uint8_t speed) {
+    #ifdef USE_RAMP_LEVEL_HARD_LIMIT
+        // don't let animations get stuck when target exceeds limit
+        if (ramp_level_hard_limit && (level > ramp_level_hard_limit))
+            smooth_steps_target = ramp_level_hard_limit;
+        else
+    #endif
     smooth_steps_target = level;
     smooth_steps_speed = speed;  // higher = slower
     smooth_steps_in_progress = 1;
     // for setting prev_level after animation ends
     smooth_steps_start = actual_level;
+
 }
 
 #endif
