b289925cafa7811644331b344d92f5d950509c7c
Selene ToyKeeper <git@toykeeper.net>
2026-09-13 09:25:58 -0600

    tested exactly which DFP versions work or fail, and updated scripts
    The newest which works on attiny1634 is 3.1.260, from 2023-08-16.
    
    For avr32dd, every version I tried produces exactly the same binaries,
    so it doesn't matter which one is used.
    
    Also made it possible to set a DFP path as an environment variable,
    so all git branches/worktrees will use the same one, to avoid issues
    where it doesn't work due to using the wrong DFP.
    
    In your shell rc (~/.bashrc or similar), do something like...
    ```sh
    export ANDURIL_DFP_PATH="$HOME/src/anduril/arch/dfp"
    ```

--- bin/build.sh
+++ bin/build.sh
@@ -29,12 +29,14 @@ MODEL_NUMBER=$(head -1 "$MODEL/model" | sed 'y/abcdefABCDEF/:;<=>?:;<=>?/')
 eval "$( bin/detect-mcu.sh "$TARGET" )"
 
 # detect and enable a relevant Atmel DFP
+DFPBASE="${BASEDIR}/arch/dfp"
+[ -n "$ANDURIL_DFP_PATH" ] && DFPBASE="$ANDURIL_DFP_PATH"
 if [[ $MCUNAME =~ "attiny1616" ]]; then
-  DFPPATH=$BASEDIR/arch/dfp/attiny1616
+  DFPPATH="${DFPBASE}/attiny1616"
 elif [[ $MCUNAME =~ "attiny" ]]; then
-  DFPPATH=$BASEDIR/arch/dfp/attiny
+  DFPPATH="${DFPBASE}/attiny"
 elif [[ $MCUNAME =~ "avr" && $MCUNAME =~ "dd" ]]; then
-  DFPPATH=$BASEDIR/arch/dfp/avrdd
+  DFPPATH="${DFPBASE}/avrdd"
 else
   echo "Unrecognized MCU type: '$MCUNAME'"
   exit 1
--- bin/dfp-install.sh
+++ bin/dfp-install.sh
@@ -5,18 +5,34 @@
 
 set -e
 
-# attiny1634 won't boot with the newer Microchip DFP, thanks Microchip
-V_ATTINY='2.0.368'
-F_ATTINY="Atmel.ATtiny_DFP.${V_ATTINY}.atpack"
+URL_ATMEL="http://packs.download.atmel.com/"
+URL_MCHIP="https://packs.download.microchip.com/"
 
+# attiny1634 / attiny85
+# attiny1634 won't boot with the newer DFPs, thanks Microchip/Atmel
+# 3.1.260 (2023-08-16) is the last version which works
+#V_ATTINY='2.0.368'  # works, 2022-03-02
+#V_ATTINY='2.1.484'  # fails, 2026-04-11
+#F_ATTINY="Atmel.ATtiny_DFP.${V_ATTINY}.atpack"
+#URL_ATTINY="${URL_ATMEL}${F_ATTINY}"
+V_ATTINY='3.1.260'  # works, 2023-08-16
+#V_ATTINY='3.2.268'  # fails, 2025-01-13
+F_ATTINY="Microchip.ATtiny_DFP.${V_ATTINY}.atpack"
+URL_ATTINY="${URL_MCHIP}${F_ATTINY}"
+
+# attiny1616
 V_ATTINY1616='3.4.278'
 F_ATTINY1616="Microchip.ATtiny_DFP.${V_ATTINY1616}.atpack"
+URL_ATTINY1616="${URL_MCHIP}${F_ATTINY1616}"
 
-V_AVRDD='2.8.343'
+# avr32dd20
+#V_AVRDD='2.2.253'  # works, 2022-12-16
+#F_AVRDD="Atmel.AVR-Dx_DFP.${V_AVRDD}.atpack"
+#URL_AVRDD="${URL_ATMEL}${F_AVRDD}"
+V_AVRDD='2.8.343'  # works, 2026-04-11 (makes same binaries as 2.2.253)
 F_AVRDD="Microchip.AVR-Dx_DFP.${V_AVRDD}.atpack"
+URL_AVRDD="${URL_MCHIP}${F_AVRDD}"
 
-OLDURL="http://packs.download.atmel.com/"
-BASEURL="https://packs.download.microchip.com/"
 
 cd arch
 
@@ -27,9 +43,9 @@ cd dfp
 # download the zipped pack files if they don't exist yet
 mkdir -p zip
 cd zip
-[ ! -f "${F_ATTINY}"     ] && wget "${OLDURL}${F_ATTINY}"
-[ ! -f "${F_ATTINY1616}" ] && wget "${BASEURL}${F_ATTINY1616}"
-[ ! -f "${F_AVRDD}"      ] && wget "${BASEURL}${F_AVRDD}"
+[ ! -f "${F_ATTINY}"     ] && wget "${URL_ATTINY}"
+[ ! -f "${F_ATTINY1616}" ] && wget "${URL_ATTINY1616}"
+[ ! -f "${F_AVRDD}"      ] && wget "${URL_AVRDD}"
 
 # extract the packs
 
